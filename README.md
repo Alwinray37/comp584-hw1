493af1c4ef2bb662a9236ca99190b9ef45552296
alwinray37 <alwinray37@gmail.com>
2026-09-08 22:30:34 -0700

    final draft

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+# COMP 584 Homework 1
+alwin ray roble
+
+live site: https://alwinray37.github.io/comp584-hw1/
+
+old link: 
 https://csun-orm.github.io/26-Fall-Comp584-hw1/
--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-console.log("Hello, world!");
+let container = document.querySelector("container");
+let clearButton = document.querySelector("#clearContainer");
+
+for (let i = 0; i < 2500; i++) {
+    let square = document.createElement("square");
+    square.addEventListener("mousedown", changeColor);
+    square.addEventListener("mouseenter", event => {
+        if (event.buttons === 1) {
+            changeColor(event);
+        }
+    });
+
+    container.appendChild(square);
+}
+
+function changeColor(event) {
+    event.target.style.backgroundColor = "blue";
+}
+
+clearButton.addEventListener("click", clearContainer);
+    function clearContainer() {
+    let squares = document.querySelectorAll("square");
+    squares.forEach(square => {
+        square.style.backgroundColor = "whitesmoke";
+    });
+}
